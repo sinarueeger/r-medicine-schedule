cb398a2ab23f81f9221edbea8eeed323c1319697
Sina Rueger <sina.rueeger@gmail.com>
2020-08-21 12:50:22 +0300

    initialize

---
+++ app.R
@@ -0,0 +1,156 @@
+source("utils.R")
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+
+ui <-
+  navbarPage(
+    "Change timezone for R/Medicine",
+    theme = shinytheme("flatly"),
+    tabPanel(
+      "Schedule",
+      # Sidebar layout with input and output definitions ----
+      sidebarLayout(
+        sidebarPanel(
+          # Input: Select a dataset ----
+          selectInput(
+            "dataset",
+            "Choose a day:",
+            choices = c("2020-08-27 (Friday)", "2020-08-28 (Saturday)")
+          ),
+          
+          
+          # Input: Specify timezone ----
+          selectizeInput(
+            'tz',
+            label = NULL,
+            choices = OlsonNames(),
+            options = list(create = TRUE)
+          ),
+          
+          # Include clarifying text ----
+          helpText("Select a day (in EDT) and your preferred time zone"),
+          
+          # Button
+          downloadButton("downloadData", "Download"),
+          
+          # Input: actionButton() to defer the rendering of output ----
+          # until the user explicitly clicks the button (rather than
+          # doing it immediately when inputs change). This is useful if
+          # the computations required to render output are inordinately
+          # time-consuming.
+          actionButton("update", "Update View")
+          
+        ),
+        # Main panel for displaying outputs ----
+        mainPanel(# Output: Header + table of distribution ----
+                  h4("Schedule"),
+                  tableOutput("view"))
+      )
+      ),
+      tabPanel("About R/Medicine",
+               h2("About R/Medicine"),
+               fluidPage(HTML("<p>For all details about the R/Medicine conference, see <a href='https://events.linuxfoundation.org/r-medicine/program/schedule/'>https://events.linuxfoundation.org/r-medicine/program/schedule/</a></p>")
+               ))
+    )
+    
+
+    # # Define UI for dataset viewer app ----
+    # ui <- fluidPage(
+    
+    #     # App title ----
+    #     titlePanel("Change timezone for R/Medicine"),
+    #
+    #     # Sidebar layout with input and output definitions ----
+    #     sidebarLayout(
+    #
+    #         # Sidebar panel for inputs ----
+    #         sidebarPanel(
+    #
+    #             # Input: Select a dataset ----
+    #             selectInput("dataset", "Choose a day:",
+    #                         choices = c("2020-08-27 (Friday)", "2020-08-28 (Saturday)")),
+    #
+    #
+    #             # Input: Specify timezone ----
+    #             selectizeInput(
+    #               'tz', label = NULL, choices = OlsonNames(),
+    #               options = list(create = TRUE)
+    #             ),
+    #
+    #             # Include clarifying text ----
+    #             helpText("Select a day (in EDT) and your preferred time zone"),
+    #
+    #             # Button
+    #             downloadButton("downloadData", "Download"),
+    #
+    #             # Input: actionButton() to defer the rendering of output ----
+    #             # until the user explicitly clicks the button (rather than
+    #             # doing it immediately when inputs change). This is useful if
+    #             # the computations required to render output are inordinately
+    #             # time-consuming.
+    #             actionButton("update", "Update View")
+    #
+    #         ),
+    #
+    #         # Main panel for displaying outputs ----
+    #         mainPanel(
+    #
+    #             # Output: Header + table of distribution ----
+    #             h4("Schedule"),
+    #             tableOutput("view")
+    #         )
+    #
+    #     )
+    # )
+    
+    # Define server logic to summarize and view selected dataset ----
+    server <- function(input, output) {
+      # Return the requested dataset ----
+      # Note that we use eventReactive() here, which depends on
+      # input$update (the action button), so that the output is only
+      # updated when the user clicks the button
+      datasetInput <- eventReactive(input$update, {
+        switch(
+          input$dataset,
+          "2020-08-27 (Friday)" = update_schedule(
+            link = "https://docs.google.com/spreadsheets/d/1248vzzKb8paaytPGwwoxEXbF0Ctn1Y3n5IgZxqJdy6c/edit#gid=1253542401",
+            sheet = "2020-08-27",
+            tz_goal = input$tz
+          ),
+          "2020-08-28 (Saturday)" = update_schedule(
+            link = "https://docs.google.com/spreadsheets/d/1248vzzKb8paaytPGwwoxEXbF0Ctn1Y3n5IgZxqJdy6c/edit#gid=1253542401",
+            sheet = "2020-08-28",
+            tz_goal = input$tz
+          )
+        )
+        
+        
+        
+      }, ignoreNULL = FALSE)
+      
+      
+      # Show the first "n" observations ----
+      # The use of isolate() is necessary because we don't want the table
+      # to update whenever input$obs changes (only when the user clicks
+      # the action button)
+      output$view <- renderTable({
+        datasetInput()
+      },   striped = TRUE, width = "auto")
+      
+      # Downloadable csv of selected dataset ----
+      output$downloadData <- downloadHandler(
+        filename = function() {
+          paste(input$dataset, ".csv", sep = "")
+        },
+        content = function(file) {
+          write.csv(datasetInput(), file, row.names = FALSE)
+        }
+      )
+      
+      
+    }
+    
+    # Create Shiny app ----
+    shinyApp(ui, server)
+    
---
+++ utils.R
@@ -0,0 +1,110 @@
+library(lubridate)
+library(googlesheets4)
+library(dplyr)
+library(tidyr)
+
+
+#' Swap times from one to another time zone
+#'
+#' @param time in format: 2020-08-27 11:00:00
+#' @param tz_time the timezone the time is in
+#' @param tz_goal the timezone you want
+#'
+#' @return updated timezone
+#' @export
+#'
+#' @examples
+#' switch_timezone(time = "2020-08-27 11:00", tz_goal = "Europe/Helsinki")
+#' switch_timezone(time = "2020-08-27 11:00:00", tz_goal = "Europe")
+switch_timezone <-
+  function(time, tz_goal) {
+
+    my_timezone <-
+      grep(tz_goal, OlsonNames(), value = TRUE)
+    
+    if (length(my_timezone) == 0) {
+      warning("Your string did not match any name in OlsonNames(). Try another one.")
+    }
+    if (length(my_timezone) > 1) {
+      warning(
+        "Your string matched more than one name in OlsonNames(): ",
+        paste(my_timezone, collapse = ', '),
+        ". Be more specific."
+      )
+    }
+    
+    my_time <- with_tz(time, tzone = my_timezone)
+    
+    return(my_time)
+    #pb.txt <- "2020-08-27 11:00"
+    #pb.date <- as.POSIXct(pb.txt, tz="US/Eastern")
+    #format(pb.date, tz=my_timezone, usetz=TRUE)
+  }
+
+
+import_schedule <- function(link, sheet, tz_schedule = "US/Eastern") {
+  dat <- read_sheet(link, sheet = sheet, col_names = TRUE)
+
+  ## special treatment for Birds of a Feather Sessions
+  ind <- grep("Birds of a Feather Sessions", dat$talk_speaker)
+  if(length(ind) > 0) for (idx in ind) dat[idx, "talk_speaker"] <- gsub("\n", "#", dat[idx, "talk_speaker"] )
+  
+  ## take columns apart
+  dat <-dat %>% 
+    separate(talk_speaker, c("talk", "speaker"), sep = "\n") %>% 
+    separate(from_to, c("from", "to"), sep = " – ")
+  
+  dat$speaker[is.na(dat$speaker)] <- ""
+  
+  ## add again the \n for birds
+  if(length(ind) > 0) for (idx in ind) dat[idx, "talk"] <- gsub("#", "\n", dat[idx, "talk"] )
+  
+  
+  ## change time format
+  dat$from <- ymd_hm(paste0(sheet, dat$from), tz = tz_schedule)
+  dat$to <- ymd_hm(paste0(sheet, dat$to), tz = tz_schedule)
+  
+  return(dat)
+}
+
+dat <- import_schedule(
+  link = "https://docs.google.com/spreadsheets/d/1248vzzKb8paaytPGwwoxEXbF0Ctn1Y3n5IgZxqJdy6c/edit#gid=1253542401", 
+  sheet = "2020-08-27")
+
+time_to_hour_minute <- function(time) {
+  format(as.POSIXct(time,format="%H:%M:%S"),"%H:%M")
+}
+
+update_schedule <- function(link, sheet, tz_goal) {
+  dat <- import_schedule(
+    link = link, 
+    sheet = sheet)
+  dat$from_my <- switch_timezone(dat$from, tz_goal)
+  dat$to_my <- switch_timezone(dat$to, tz_goal)
+  
+  ## combine from - to
+  dat <- dat %>% mutate(from = time_to_hour_minute(from),
+                        to = time_to_hour_minute(to),
+                        from_my = time_to_hour_minute(from_my), 
+                        to_my = time_to_hour_minute(to_my)) %>% 
+    unite("time", c("from", "to"), sep = " - ") %>%
+    unite("my_time", c("from_my", "to_my"), sep = " - ")
+
+  
+  ## order
+  dat <- dat %>% select(`Time`= my_time, Talk = talk, Speaker = speaker, `Time (EDT)`= time)
+
+  return(dat)  
+}
+
+dat <- update_schedule(
+  link = "https://docs.google.com/spreadsheets/d/1248vzzKb8paaytPGwwoxEXbF0Ctn1Y3n5IgZxqJdy6c/edit#gid=1253542401", 
+  sheet = "2020-08-28",
+  tz_goal = "Europe/Helsinki"
+  )
+
+
+
+
+
+
